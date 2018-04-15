c4210771b7b656fdc4a8603d5912b265049dc7e5
Vyacheslav <slavcezo@gmail.com>
2018-04-15 19:55:00 +0300

    удалены лишние файлы

--- RunJade.bat
+++
@@ -1,3 +0,0 @@
-set JADE_HOME=c:\jade
-set CLASSPATH=%JADE_HOME%\lib\jade.jar; %JADE_HOME%\lib\jadeTools.jar; %JADE_HOME%\lib\http.jar; %JADE_HOME%\lib\iiop.jar; %JADE_HOME%\lib\commons-codec\commons-codec-1.3.jar; %JADE_HOME%\classes
-java jade.Boot -gui
